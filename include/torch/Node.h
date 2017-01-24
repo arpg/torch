db30cf5167fb8596ea1064c52cbcb980ac71b5e3
Mike Kasper <michael.kasper@colorado.edu>
2017-01-24 14:36:24 -0700

    added children vector to node

--- include/torch/Node.h
+++ include/torch/Node.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <torch/Object.h>
 
 namespace torch
@@ -12,6 +13,26 @@ class Node : public Object
     Node(std::shared_ptr<Context> context);
 
     ~Node();
+
+    size_t GetChildCount() const;
+
+    std::shared_ptr<Node> GetChild(size_t index) const;
+
+    bool HasChild(std::shared_ptr<const Node> child) const;
+
+    void AddChild(std::shared_ptr<Node> child);
+
+    void RemoveChild(std::shared_ptr<const Node> child);
+
+    void RemoveChildren();
+
+  protected:
+
+    bool CanAddChild(std::shared_ptr<const Node> child) const;
+
+  protected:
+
+    std::vector<std::shared_ptr<Node>> m_children;
 };
 
 } // namespace torch
--- src/Node.cpp
+++ src/Node.cpp
@@ -1,4 +1,6 @@
 #include <torch/Node.h>
+#include <algorithm>
+#include <torch/Context.h>
 #include <torch/Object.h>
 
 namespace torch
@@ -11,6 +13,56 @@ Node::Node(std::shared_ptr<Context> context) :
 
 Node::~Node()
 {
+}
+
+size_t Node::GetChildCount() const
+{
+  return m_children.size();
+}
+
+std::shared_ptr<Node> Node::GetChild(size_t index) const
+{
+  return m_children[index];
+}
+
+bool Node::HasChild(std::shared_ptr<const Node> child) const
+{
+  auto iter = std::find(m_children.begin(), m_children.end(), child);
+  return iter != m_children.end();
+}
+
+void Node::AddChild(std::shared_ptr<Node> child)
+{
+  if (CanAddChild(child))
+  {
+    m_children.push_back(child);
+    m_context->MarkDirty();
+  }
+}
+
+void Node::RemoveChild(std::shared_ptr<const Node> child)
+{
+  auto iter = std::find(m_children.begin(), m_children.end(), child);
+
+  if (iter != m_children.end())
+  {
+    m_children.erase(iter);
+    m_context->MarkDirty();
+  }
+}
+
+void Node::RemoveChildren()
+{
+  if (!m_children.empty())
+  {
+    m_children.clear();
+    m_context->MarkDirty();
+  }
+}
+
+bool Node::CanAddChild(std::shared_ptr<const Node> child) const
+{
+  return child && child.get() != this && !HasChild(child);
 }
 
 } // namespace torch
